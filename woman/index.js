774eafad49560ec793ba2ed3db6f9452315052f9
Andrew Wiltshire <62200778+AW1534@users.noreply.github.com>
2022-10-12 16:02:49 +0100

    Initial commit

---
+++ woman/index.js
@@ -0,0 +1,4 @@
+export default function() {
+    const text = ["put the toilet seat down", "I had a dream that you cheated on me.", "im not hungry *takes all of your food*"]
+    console.log(text[Math.floor(Math.random() * text.length)])
+}
