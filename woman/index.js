c0b8785fd3a9c4b1efc7a8ed8ba249e9126da096
Lance Ross <102563271+lancerossdev@users.noreply.github.com>
2022-10-19 17:10:42 +0800

    new phrases and update

--- woman/index.js
+++ woman/index.js
@@ -1,4 +1,4 @@
-export default function() {
-    const text = ["put the toilet seat down", "I had a dream that you cheated on me.", "im not hungry *takes all of your food*"]
-    console.log(text[Math.floor(Math.random() * text.length)])
+export default function () {
+    const text = ["Put the toilet seat down", "I had a dream that you cheated on me. 😾", "I'm not hungry *takes all of your food*", "I can't stop thinking about you.", "Babe, let's go to the mall.", "Babe, do you love me? 🥺", "Aww, you're so sweet! 🥰"];
+    console.log(text[Math.floor(Math.random() * text.length)]);
 }
