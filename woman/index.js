7c890983e4e5d362be2ffe2e392bde33996e5244
Shifat Bin Rahman <87413554+Shifat7@users.noreply.github.com>
2023-01-18 12:16:13 +1100

    formatted code and DRY principle, alon with a new text element

--- woman/index.js
+++ woman/index.js
@@ -1,4 +1,15 @@
 export default function () {
-    const text = ["Put the toilet seat down", "I had a dream that you cheated on me. 😾", "I'm not hungry *takes all of your food*", "I can't stop thinking about you.", "Babe, let's go to the mall.", "Babe, do you love me? 🥺", "Aww, you're so sweet! 🥰"];
-    console.log(text[Math.floor(Math.random() * text.length)]);
+    const text = [
+    "Put the toilet seat down", 
+    "I had a dream that you cheated on me. 😾", 
+    "I'm not hungry *takes all of your food*", 
+    "I can't stop thinking about you.", 
+    "Babe, let's go to the mall.", 
+    "Babe, do you love me? 🥺", 
+    "Aww, you're so sweet! 🥰", 
+    "Babe would you still love me if I was a Lamborghini Aventador LP 750-4 Superveloce Roadster"
+];
+    const RandText = text[Math.floor(Math.random() * text.length)]
+    console.log(RandText);
+    return RandText
 }
